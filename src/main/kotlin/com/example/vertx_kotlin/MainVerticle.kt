64b4c72edd0a4df60aa290f235fb9982ad44fd01
minatorak <hotmail22866@gmail.com>
2021-12-13 15:16:53 +0700

    routing with function

--- src/main/kotlin/com/example/vertx_kotlin/MainVerticle.kt
+++ src/main/kotlin/com/example/vertx_kotlin/MainVerticle.kt
@@ -2,16 +2,26 @@ package com.example.vertx_kotlin
 
 import com.example.vertx_kotlin.commons.mainRouter
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.AsyncResult
 import io.vertx.core.Promise
+import io.vertx.core.http.HttpServer
 import io.vertx.core.http.HttpServerOptions
 
 class MainVerticle : AbstractVerticle() {
 
-  override fun start(startPromise: Promise<Void>) {
-    val server = vertx.createHttpServer(HttpServerOptions().apply {
+  private val server by lazy { vertx.createHttpServer(HttpServerOptions()) }
 
-    })
-    server.requestHandler(mainRouter(vertx)).listen(8080)
+  override fun start(startPromise: Promise<Void>) {
+    server
+      .requestHandler(mainRouter(vertx))
+      .listen(8080) { httpServe: AsyncResult<HttpServer> ->
+        if (httpServe.succeeded()) {
+          println("HTTP server started on port 8888")
+          startPromise.complete()
+        } else {
+          startPromise.fail(httpServe.cause())
+        }
+      }
   }
 
   override fun stop(stopPromise: Promise<Void>) {
--- src/main/kotlin/com/example/vertx_kotlin/commons/MainRounting.kt
+++ src/main/kotlin/com/example/vertx_kotlin/commons/MainRounting.kt
@@ -1,39 +1,45 @@
 package com.example.vertx_kotlin.commons
 
+import io.vertx.core.Handler
 import io.vertx.core.Vertx
+import io.vertx.core.http.HttpMethod.GET
+import io.vertx.core.http.HttpMethod.POST
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.CorsHandler
 import io.vertx.ext.web.handler.TimeoutHandler
 import java.time.Duration
 
-fun mainRouter(vertx: Vertx): Router {
-  val router = Router.router(vertx)
-  router.route().handler(::loggerHandler)
+fun mainRouter(vertx: Vertx): Router = Router.router(vertx).apply {
+  route().handler(loggerHandler)
+  route().handler(cors)
 
-  router.route().handler(TimeoutHandler.create(Duration.ofSeconds(2).toMillis()))
-  router.post().failureHandler(::helloFailureHandler)
+  route().handler(TimeoutHandler.create(Duration.ofSeconds(2).toMillis()))
+  post().failureHandler(helloFailureHandler)
 
-  router.post("/hello").handler(::helloHandler)
-  return router
+  post("/hello").handler(helloHandler)
 }
 
-private fun loggerHandler(routingContext: RoutingContext) {
-  routingContext.request().body {
+val loggerHandler = Handler { ctx: RoutingContext ->
+  ctx.request().body {
     println("loggerHandler ${it.result()}")
   }
-  routingContext.next()
+  ctx.next()
 
-  routingContext.response()
 }
 
-private fun helloFailureHandler(failureRoutingContext: RoutingContext) {
+val cors = CorsHandler.create("*")
+  .allowedHeader("content-type")
+  .allowedMethods(setOf(GET, POST))
+
+val helloFailureHandler = Handler { failureRoutingContext: RoutingContext  ->
   println("bodyAsString " + failureRoutingContext.bodyAsString)
   println(failureRoutingContext.failure())
   failureRoutingContext.response().statusCode = 400
   failureRoutingContext.response().send()
 }
 
-private fun helloHandler(routingContext: RoutingContext) {
+val helloHandler = Handler { routingContext: RoutingContext ->
   routingContext.request().body {
     println("helloHandler ${it.result()}")
   }
