12ebfda214e109d630e2b36b37863d97e50e2827
minatorak <hotmail22866@gmail.com>
2021-12-21 14:59:56 +0700

    start web server on coroutine context

--- src/main/kotlin/com/example/vertx_kotlin/MainVerticle.kt
+++ src/main/kotlin/com/example/vertx_kotlin/MainVerticle.kt
@@ -1,32 +1,17 @@
 package com.example.vertx_kotlin
 
-import com.example.vertx_kotlin.commons.mainRouter
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.AsyncResult
-import io.vertx.core.Promise
-import io.vertx.core.http.HttpServer
-import io.vertx.core.http.HttpServerOptions
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.await
+import kotlinx.coroutines.launch
 
-class MainVerticle : AbstractVerticle() {
+class MainVerticle :CoroutineVerticle() {
 
-  private val server by lazy { vertx.createHttpServer(HttpServerOptions()) }
-
-  override fun start(startPromise: Promise<Void>) {
-    server
-      .requestHandler(mainRouter(vertx))
-      .listen(8080) { httpServe: AsyncResult<HttpServer> ->
-        if (httpServe.succeeded()) {
-          println("HTTP server started on port 8888")
-          startPromise.complete()
-        } else {
-          startPromise.fail(httpServe.cause())
-        }
-      }
+  override suspend fun start() {
+    launch {  startWebServer() }
   }
 
-  override fun stop(stopPromise: Promise<Void>) {
-    super.stop(stopPromise)
+  private suspend fun startWebServer() {
+    vertx.deployVerticle(WebServer::class.qualifiedName).await()
   }
-
 
 }
---
+++ src/main/kotlin/com/example/vertx_kotlin/WebServer.kt
@@ -0,0 +1,27 @@
+package com.example.vertx_kotlin
+
+import com.example.vertx_kotlin.commons.mainRouter
+import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.await
+
+class WebServer : CoroutineVerticle() {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(WebServer::class.java)
+  }
+
+  override suspend fun start() {
+    vertx.createHttpServer()
+      .requestHandler(mainRouter(vertx))
+      .listen(config.getInteger("http.port", 8080))
+      .onSuccess {
+        log.info("HTTP server started on port " + it.actualPort())
+      }
+      .await()
+  }
+
+  override suspend fun stop() {
+    log.info("WebServer stop")
+  }
+}
