94afdd5b1aa31e4bd9738f21cffb64e0c7a6b23f
minatorak <hotmail22866@gmail.com>
2021-12-22 13:28:04 +0700

    log request response and api summary

--- src/main/kotlin/com/example/vertx_kotlin/commons/MainRounting.kt
+++ src/main/kotlin/com/example/vertx_kotlin/commons/MainRounting.kt
@@ -8,12 +8,11 @@ import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.CorsHandler
 import io.vertx.ext.web.handler.TimeoutHandler
-import io.vertx.kotlin.coroutines.awaitResult
 import java.time.Duration
 
 fun mainRouter(vertx: Vertx): Router = Router.router(vertx).apply {
-  route().handler(loggerHandler)
   route().handler(cors)
+  route().handler(loggerHandler)
 
   route().handler(TimeoutHandler.create(Duration.ofSeconds(2).toMillis()))
   post().failureHandler(helloFailureHandler)
@@ -29,68 +28,10 @@ val loggerHandler = Handler { ctx: RoutingContext ->
   ctx.addHeadersEndHandler {
     ctx.request().bodyHandler {
       println("addHeadersEndHandler ${ctx.get<String>("response")}")
+      println("log api summary ${System.currentTimeMillis() - ctx.get<Long>("timestamp")}")
     }
   }
-
-//  ctx.request().handler {requestBuffer->
-//    println("request().handler $requestBuffer")
-//  }
-//
-//  ctx.addBodyEndHandler {
-//    println("addBodyEndHandler ${ctx.get<String>("response")}")
-//  }
-//  ctx.addEndHandler {
-//    ctx.request().bodyHandler { requestBuffer ->
-//      println("addEndHandler $requestBuffer + ${ctx.get<String>("response")}")
-//    }
-//  }
-//
-//  ctx.addHeadersEndHandler {
-//    ctx.request().bodyHandler { requestBuffer ->
-//      println("addHeadersEndHandler $requestBuffer + ${ctx.get<String>("response")}")
-//    }
-//  }
-//
-//  ctx.addBodyEndHandler {
-//    ctx.request().bodyHandler { requestBuffer ->
-//      println("addHeadersEndHandler $requestBuffer + ${ctx.get<String>("response")}")
-//    }
-//  }
-//
-//  ctx.response().closeHandler {
-//    ctx.request().bodyHandler { requestBuffer ->
-//      println("response().closeHandler $requestBuffer + ${ctx.get<String>("response")}")
-//    }
-//  }
-//  ctx.request().handler {
-//      println("request().endHandler $it + ${ctx.get<String>("response")}") // 4
-//  }
-//
-//  ctx.request().bodyHandler {
-//    println("bodyHandler ${it}") // 5
-//    println("bodyHandler response-> ${ctx.get<String>("response")}") // 6
-//  }
-//
-//  ctx.request().body {
-//    println("loggerHandler ${it.result()}") // 7
-//  }
-//    ctx.request().body().onSuccess {
-//      println("ctx.request().body().onSuccess")
-//    }.onComplete {
-//      println("ctx.request().body().onComplete")
-//    }
-//  ctx.addHeadersEndHandler {
-//    ctx.request().body().onComplete {
-//      println("addHeadersEndHandler request -> $it") // 9
-//    }
-//    println("addHeadersEndHandler response-> ${ctx.get<String>("response")}") // 1
-//  }
-//  ctx.addEndHandler {
-//    println("addEndHandler -> ${ctx.get<String>("response")}") // 3
-//  }
-//  ctx.response().bodyEndHandler {
-//    println("closeHandler -> ${ctx.get<String>("response")}") // 2
-//  }
+  ctx.put("timestamp", System.currentTimeMillis())
   ctx.next()
 
 }
