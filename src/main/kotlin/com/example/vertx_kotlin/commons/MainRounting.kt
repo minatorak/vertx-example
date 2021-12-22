a65573a2ba91c3de2fbd7159207aad5c206f79b8
minatorak <hotmail22866@gmail.com>
2021-12-22 13:07:54 +0700

    prove step print log request -> service -> response

--- src/main/kotlin/com/example/vertx_kotlin/commons/MainRounting.kt
+++ src/main/kotlin/com/example/vertx_kotlin/commons/MainRounting.kt
@@ -8,6 +8,7 @@ import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.CorsHandler
 import io.vertx.ext.web.handler.TimeoutHandler
+import io.vertx.kotlin.coroutines.awaitResult
 import java.time.Duration
 
 fun mainRouter(vertx: Vertx): Router = Router.router(vertx).apply {
@@ -21,9 +22,75 @@ fun mainRouter(vertx: Vertx): Router = Router.router(vertx).apply {
 }
 
 val loggerHandler = Handler { ctx: RoutingContext ->
-  ctx.request().body {
-    println("loggerHandler ${it.result()}")
+  ctx.request().body().onComplete {
+    println("request -> ${it.result()}")
   }
+
+  ctx.addHeadersEndHandler {
+    ctx.request().bodyHandler {
+      println("addHeadersEndHandler ${ctx.get<String>("response")}")
+    }
+  }
+
+//  ctx.request().handler {requestBuffer->
+//    println("request().handler $requestBuffer")
+//  }
+//
+//  ctx.addBodyEndHandler {
+//    println("addBodyEndHandler ${ctx.get<String>("response")}")
+//  }
+//  ctx.addEndHandler {
+//    ctx.request().bodyHandler { requestBuffer ->
+//      println("addEndHandler $requestBuffer + ${ctx.get<String>("response")}")
+//    }
+//  }
+//
+//  ctx.addHeadersEndHandler {
+//    ctx.request().bodyHandler { requestBuffer ->
+//      println("addHeadersEndHandler $requestBuffer + ${ctx.get<String>("response")}")
+//    }
+//  }
+//
+//  ctx.addBodyEndHandler {
+//    ctx.request().bodyHandler { requestBuffer ->
+//      println("addHeadersEndHandler $requestBuffer + ${ctx.get<String>("response")}")
+//    }
+//  }
+//
+//  ctx.response().closeHandler {
+//    ctx.request().bodyHandler { requestBuffer ->
+//      println("response().closeHandler $requestBuffer + ${ctx.get<String>("response")}")
+//    }
+//  }
+//  ctx.request().handler {
+//      println("request().endHandler $it + ${ctx.get<String>("response")}") // 4
+//  }
+//
+//  ctx.request().bodyHandler {
+//    println("bodyHandler ${it}") // 5
+//    println("bodyHandler response-> ${ctx.get<String>("response")}") // 6
+//  }
+//
+//  ctx.request().body {
+//    println("loggerHandler ${it.result()}") // 7
+//  }
+//    ctx.request().body().onSuccess {
+//      println("ctx.request().body().onSuccess")
+//    }.onComplete {
+//      println("ctx.request().body().onComplete")
+//    }
+//  ctx.addHeadersEndHandler {
+//    ctx.request().body().onComplete {
+//      println("addHeadersEndHandler request -> $it") // 9
+//    }
+//    println("addHeadersEndHandler response-> ${ctx.get<String>("response")}") // 1
+//  }
+//  ctx.addEndHandler {
+//    println("addEndHandler -> ${ctx.get<String>("response")}") // 3
+//  }
+//  ctx.response().bodyEndHandler {
+//    println("closeHandler -> ${ctx.get<String>("response")}") // 2
+//  }
   ctx.next()
 
 }
@@ -32,7 +99,7 @@ val cors = CorsHandler.create("*")
   .allowedHeader("content-type")
   .allowedMethods(setOf(GET, POST))
 
-val helloFailureHandler = Handler { failureRoutingContext: RoutingContext  ->
+val helloFailureHandler = Handler { failureRoutingContext: RoutingContext ->
   println("bodyAsString " + failureRoutingContext.bodyAsString)
   println(failureRoutingContext.failure())
   failureRoutingContext.response().statusCode = 400
@@ -41,10 +108,12 @@ val helloFailureHandler = Handler { failureRoutingContext: RoutingContext  ->
 
 val helloHandler = Handler { routingContext: RoutingContext ->
   routingContext.request().body {
-    println("helloHandler ${it.result()}")
+    println("service ${it.result()}") // 8
   }
-  throw RuntimeException()
-
+  val responseBody = "Hello"
+  routingContext.put("response", responseBody)
+  routingContext
+    .response().end(responseBody)
 }
 
 
